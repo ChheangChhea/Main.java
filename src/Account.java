0e3af5a2ff2da310a633eee232774a547ec879a6
Chheang.chhea <chheangchhea008@gmail.com>
2023-03-21 08:17:13 +0700

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+
+    int id;
+    String ownerName;
+    double balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
